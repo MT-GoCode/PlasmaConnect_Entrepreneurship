cfcb20c4334f42bb41f1de318a2940df3f964ecf
Minh Trinh <tminh.us@gmail.com>
2020-07-08 02:06:50 -0700

    adding localhost to the link

--- client/src/pages/ModernBusiness/pages/CenterSignUp.js
+++ client/src/pages/ModernBusiness/pages/CenterSignUp.js
@@ -38,7 +38,22 @@ class CenterSignUp extends Component {
     }
     
     componentWillMount = () => {
-        axios.post('/getcenters')
+        axios.post('http://localhost:4000/getcenters')
+        .then(res => {
+            console.log(res.data)
+            console.log(res)
+            // let centers = res.data.markers.marker
+            // // let centers = res.data[0].marker
+            // // // console.log(centers)
+            // let toState = centers.map((center) => {
+            //     return {
+            //         value: center.name + ' - '+ center.address,
+            //         label: center.name + ' - '+ center.address}
+            // })
+            // // console.log(toState);
+            // this.setState({options: toState})
+        });
+        axios.post('http://https://plasmaconnect.herokuapp.com:4000/getcenters')
         .then(res => {
             console.log(res.data)
             console.log(res)
