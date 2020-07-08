da99bdde77b1e866d026a0c54bbbdf0418e565c5
Minh Trinh <tminh.us@gmail.com>
2020-07-08 01:56:23 -0700

    im a teapot v2

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ const server = app.listen(port, () => {
 })
 
 app.get("/getcenters", (req, res) => {
-  res.send(418)
+  res.sendStatus(418)
   // axios.get('https://www.donatingplasma.org/index.php?option=com_storelocator&view=map&format=raw&searchall=1')
   //     .then(data => {
   //       parseString(data.data, function (err, result) {
